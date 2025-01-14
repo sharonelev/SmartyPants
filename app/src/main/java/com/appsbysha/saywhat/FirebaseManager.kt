1fb329d463be20a6e4c8d4f6ef2412467b8dd099
sharonelev <sha17187@gmail.com>
2025-01-14 10:06:48 +0200

    Add files via upload

--- app/src/main/java/com/appsbysha/saywhat/FirebaseManager.kt
+++ app/src/main/java/com/appsbysha/saywhat/FirebaseManager.kt
@@ -1,13 +1,17 @@
 package com.appsbysha.saywhat
 
 import android.util.Log
-import com.appsbysha.saywhat.model.Saying
 import com.appsbysha.saywhat.model.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 /**
  * Created by sharone on 07/01/2025.
@@ -56,6 +60,31 @@ suspend fun getUserData(userId: String): User? {
         null
     }
 }
+
+suspend fun listenToUserData(userId: String, onUserDataChange: (User?) -> Unit) {
+    val database = Firebase.database
+
+    withContext(Dispatchers.IO) {
+        val userRef = database.getReference("users").child(userId)
+
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dataMap = snapshot.value as? Map<String, Any>
+                val gson = Gson()
+                val jsonString = gson.toJson(dataMap)
+                val user = gson.fromJson(jsonString, User::class.java)
+                onUserDataChange(user)
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase_TEST", "Error getting data", error.toException())
+                onUserDataChange(null)
+            }
+        })
+    }
+}
+
 
 suspend fun updateSayingLine(sayingPath: String, newLine: String) {
     val database = FirebaseDatabase.getInstance()
